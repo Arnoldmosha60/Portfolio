1723d3ca0a6bb6a686f23fc9e663574fe70b03a0
Arnoldmosha60 <arnoldmosha60@gmail.com>
2024-02-28 01:31:19 +0300

    First Commit

--- src/components/BackToTopButton.jsx
+++ src/components/BackToTopButton.jsx
@@ -1,9 +1,27 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { ArrowUp } from './Icons'
+import { Link } from 'react-scroll'
 
 const BackToTopButton = () => {
+  const [btnVisibility, setBtnVisibility] = useState(false);
+  const handleScroll = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  }
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      window.pageYOffset > 250 ? setBtnVisibility(true) : setBtnVisibility(false);
+    }
+    window.addEventListener('scroll', toggleVisibility);
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility);
+    }
+  }, [])
   return (
-    <div>BackToTopButton</div>
+    <Link onClick={handleScroll} to='Home' smooth duration={300}>
+      <div className=''></div>
+    </Link>
   )
 }
 
